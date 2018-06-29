953ee1117bdef04cb1de20bf9fb809df445e6357
Pikachu <pocketfish@yeah.net>
2018-06-29 16:31:32 +0800

    ref to a paper...but quite slow

--- src/Guess.hs
+++ src/Guess.hs
@@ -1,23 +1,49 @@
-module Guess (
-  GameState, 
-  initialize, 
-  guess,
-  refine
-) where
+module Guess where
 
+import Data.List
 import System.Random
+import Text.Printf
 
--- your GameState, default to (), can be modified
-type GameState = ()
+-- Define types
+type Answer = Int
+type Response = (Int, Int)
+type GameState = [Answer] -- candidate set of answers
 
--- your initializer for GateState, can be modified
+-- Initialize the candidate set
 initialize :: (Int -> Bool) -> GameState
-initialize _ = ()
+initialize p = [x | x <- [100..9999], p x]
 
--- your guess function, implement it
-guess :: RandomGen g => g -> GameState -> (Int, GameState)
-guess g s = (9527, s)
+-- Evaluate bulls and cows
+eval :: Answer -> Answer -> Response
+eval target shot = (appeared, inPosition) where
+  show04 :: Int -> String
+  show04 = printf "%04d"
+  de = show04 target
+  dx = show04 shot
+  appeared = length $ filter (\d -> d `elem` dx) de
+  inPosition = length $ filter (\(x,y) -> x == y) $ zip (reverse de) (reverse dx)
 
--- notify the result of the last guess and refine the game state
-refine :: (Int, GameState) -> (Int, Int) -> GameState
-refine (_, s) (_, _) = s
+----------------------------------------------------------------------------------- 
+-- Choose one element in the candidate set to give the guess
+-- This specific element we choose should clean more elements in the candidate set
+-- Refer to the paper:
+-- Efficient solutions for Mastermind using genetic algorithms, page 8
+----------------------------------------------------------------------------------- 
+guess :: RandomGen g => g -> GameState -> (Answer, GameState)
+guess _ candidates = (choice, candidates) where
+  range = min 100 $ length candidates
+  choice = fst $ foldr1 (\x acc -> if snd x < snd acc then x else acc) [(c, remain c) | c <- candidates] where
+  
+  -- Compute the size of remain set of one specific answer
+  remain :: Answer -> Int
+  remain ans = length [y | x <- xs, y <- xs, eval x ans == eval y ans] where
+    xs = sample (mkStdGen ans) range candidates
+    
+-- Extract a given number of randomly selected elements from the candidate set
+sample :: RandomGen g => g -> Int -> GameState -> GameState
+sample g n l = map (l!!) is where
+  is = take n . nub $ randomRs (0, length l - 1) g
+
+-- Clean the candidate set according to the response of the last guess
+refine :: (Answer, GameState) -> Response -> GameState
+refine (ans, s) r = filter (\x -> r == eval x ans) s
