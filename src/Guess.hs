7b4fe9c07858bd8eeb9f81ae136b683d31929b55
Pikachu <pocketfish@yeah.net>
2018-06-30 11:29:22 +0800

    entropy. still slow= =

--- src/Guess.hs
+++ src/Guess.hs
@@ -1,4 +1,9 @@
-module Guess where
+module Guess (
+  GameState, 
+  initialize, 
+  guess,
+  refine
+) where
 
 import Data.List
 import System.Random
@@ -7,11 +12,11 @@ import Text.Printf
 -- Define types
 type Answer = Int
 type Response = (Int, Int)
-type GameState = [Answer] -- candidate set of answers
+type GameState = ([Answer], Int) -- candidate set of answers and the loop time
 
 -- Initialize the candidate set
 initialize :: (Int -> Bool) -> GameState
-initialize p = [x | x <- [100..9999], p x]
+initialize p = ([x | x <- [100..9999], p x], 0)
 
 -- Evaluate bulls and cows
 eval :: Answer -> Answer -> Response
@@ -23,27 +28,30 @@ eval target shot = (appeared, inPosition) where
   appeared = length $ filter (\d -> d `elem` dx) de
   inPosition = length $ filter (\(x,y) -> x == y) $ zip (reverse de) (reverse dx)
 
------------------------------------------------------------------------------------ 
--- Choose one element in the candidate set to give the guess
--- This specific element we choose should clean more elements in the candidate set
--- Refer to the paper:
--- Efficient solutions for Mastermind using genetic algorithms, page 8
------------------------------------------------------------------------------------ 
-guess :: RandomGen g => g -> GameState -> (Answer, GameState)
-guess _ candidates = (choice, candidates) where
-  range = min 100 $ length candidates
-  choice = fst $ foldr1 (\x acc -> if snd x < snd acc then x else acc) [(c, remain c) | c <- candidates] where
   
-  -- Compute the size of remain set of one specific answer
-  remain :: Answer -> Int
-  remain ans = length [y | x <- xs, y <- xs, eval x ans == eval y ans] where
-    xs = sample (mkStdGen ans) range candidates
-    
--- Extract a given number of randomly selected elements from the candidate set
-sample :: RandomGen g => g -> Int -> GameState -> GameState
-sample g n l = map (l!!) is where
-  is = take n . nub $ randomRs (0, length l - 1) g
+partitions :: [Response] -> [(Response, Int)]
+partitions = map (\xs -> (head xs, length xs)) . group . sort
+
+reactions :: Answer -> [Answer] -> [Response]
+reactions question [] = [] 
+reactions question (c:codes) = (eval question c) : reactions question codes
+
+
+entropies :: [Answer] -> [Answer]-> [(Answer, Double)]
+entropies [] _ = []
+entropies (c:cs) codes = (c, len) : entropies cs codes where
+  parts = partitions (reactions c codes)
+  xs = [x | (_,x) <- parts]
+  sumxs = sum xs
+  len = negate $ sum $ map (\x -> (fromIntegral x / fromIntegral sumxs) * logBase 2 (fromIntegral x / fromIntegral sumxs)) xs
+
+ 
+guess :: RandomGen g => g -> GameState -> (Answer, GameState)
+guess g (candidates, count) = (choice, (candidates, count)) where
+  choice = 
+    if count == 0 then 1234
+    else fst $ foldr1 (\x acc -> if snd x > snd acc then x else acc) $ entropies candidates candidates
 
 -- Clean the candidate set according to the response of the last guess
 refine :: (Answer, GameState) -> Response -> GameState
-refine (ans, s) r = filter (\x -> r == eval x ans) s
+refine (ans, (ls, t)) r = (filter (\x -> r == eval x ans) ls, t + 1)
